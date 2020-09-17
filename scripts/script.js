d78d44e245c1acc177d6979ce8826f71905fb8e9
Sayed Ibn Masud <sayedibnmasud@gmail.com>
2020-09-18 03:10:34 +0600

    Clock freezing issue fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,14 @@
-var showMilitaryTime = false;
+// show clock
 
+var idVar = setInterval(() => { 
+   timer()
+}, 1000);
 
-function showHours(theHour)
-{
-	if (showMilitaryTime || (theHour > 0 && theHour < 13))
-		return (theHour);
-	else if (theHour == 0)
-		return 12;
-	else return (theHour - 12)
-}
-
-function fillZeros(inValue)
-{
-	if (inValue > 9)
-		return ":" + inValue;
-	else return ":0" + inValue;
-}
-
-function showAmPm()
-{
-	if (showMilitaryTime)
-		return ("");
-	if (now.getHours() < 12)
-		return (" AM");
-	else return (" PM");
-}
-
-function showTime(elm)
-{
-	now = new Date;
-	
-	var theTime = 
-		showHours(now.getHours()) + 
-		fillZeros(now.getMinutes()) + 
-		fillZeros(now.getSeconds()) + 
-		showAmPm();
-  document.getElementById(elm).innerHTML = theTime;
-	setTimeout("showTime('"+elm+"')", 1000);
-}
-
-showTime('clock');
-
+function timer() {
+   var dateVar = new Date();
+   var time = dateVar.toLocaleTimeString();
+   document.getElementById("clock").innerHTML = time;
+};
 
 
 // Random Dhikr script passing
@@ -57,7 +25,7 @@ function Generate() {
     var meaning = document.getElementById('meaning');
     
 
-    $.getJSON('https://raw.githubusercontent.com/niyazed/dhikr/master/data.json', function(data) {
+    $.getJSON('https://raw.githubusercontent.com/Deenipedia/dhikr/master/data.json', function(data) {
       arabic.innerText = data[NumberResult].ARABIC
       bangla.innerText = data[NumberResult].BANGLA_UCCHARON
       meaning.innerText = data[NumberResult].BANGLA_ORTHO
