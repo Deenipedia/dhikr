17ab0bbf7b846685a049d805975dc88acb12c149
mapplee <iapplee007@gmail.com>
2022-06-05 10:53:10 +0600

    Namaj time api added

--- src/components/NamazTimes.js
+++ src/components/NamazTimes.js
@@ -1,21 +1,60 @@
-const NamazTimes = () => {
-    let post = []
-    let namazT = [5, 14, 16, 17, 20]
-    for (let i = 0; i < 24; i++) post.push(<li></li>);
-    let NNIndex = 0;
+import axios from "axios";
+import { useEffect, useState } from "react";
+import './App.css';
+import logo from './logo.svg';
+function App() {
+    let namazT=[];
+    const [data ,setData]=useState({});
+    const location = {
+        latitute:'23',
+        longitute:'90'
+    };
 
-    let namazN = ["fazr", "dhuhr", "asr", "maghrib", "isha"]
-    namazT.forEach(it => {
-        post[it - 1] = <li><b>{namazN[NNIndex]}</b> {it > 12 ? (it - 12) + ":00" : it + ":00"}
-            <apm>{it > 12 ? "pm" : "am"}</apm>
-        </li>;
+   // const url =`https://jsonplaceholder.typicode.com/users`
+    const url =`http://api.aladhan.com/v1/timings/1398332113?latitude=${location.latitute}&longitude=${location.longitute}&method=2`
+    useEffect( ()=>{
+        async function getNamajTime()
+        {
+            const result=await axios(url);
+            //namazT=Object.values(result.data.timings);
+            setData(result.data);
+            //setData(result.data);
 
-        NNIndex++;
-    });
+        };
 
-    return <div className="ll-clock-holder"><h4>Namaz Times</h4>
-        <ul>{post}</ul>
+         getNamajTime();
+    },[])
+    let tmp={};
+
+    tmp={
+        ...data.data?.timings
+    };
+    namazT=Object.values(tmp);
+    console.log(namazT);
+
+
+
+  return (
+    <div className="App">
+
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-};
+  );
+}
 
-export default NamazTimes
+export default App;
