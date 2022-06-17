f2964c31ebee35e8714726c28c0efba8390ecf19
mapplee <iapplee007@gmail.com>
2022-06-18 02:55:51 +0600

    Random Hadith

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,14 +1,37 @@
-const Quiz = () => (
-    <div className="right-quiz">
-        <div className="rq-content">
-            <div className="rq-content-text">
-                <p>Coming Soon.</p>
+import {useEffect, useState} from "react";
+
+const URL = "https://raw.githubusercontent.com/NHSanto/dhikr/main/public/hadith.json";
+
+const Quiz = () => {
+    const [hadith, setHadith] = useState([]);
+
+    useEffect(() => {
+        fetch(URL)
+            .then(res => res.json())
+            .then(list => {
+                const listLength=Object.keys(list).length;
+                return list[Math.floor(Math.random() * listLength)]
+            })
+            .then(setHadith)
+
+    }, []);
+
+    function randomHadith() {
+        return {
+            __html: hadith   };
+    }
+    return hadith &&(
+        <div className="right-quiz">
+            <div className="rq-content">
+                <div className="rq-content-text">
+
+                    <div dangerouslySetInnerHTML={randomHadith()} />
+                </div>
+                <h6>Learn New Words</h6>
             </div>
-            <h6>Learn New Words</h6>
         </div>
-    </div>
+    )
 
-);
-
+};
 
 export default Quiz;
