cb34bac1192b1b20828e862ca41766dd1e7d2c1a
mapplee <iapplee007@gmail.com>
2022-06-21 03:35:13 +0600

    update build.zip

--- src/components/Shortcuts.js
+++ src/components/Shortcuts.js
@@ -3,7 +3,6 @@ import {ChromeContext} from "../Contexts";
 
 const shorten = text => text.length > 14 ? text.slice(0, 12) + '...' : text
 const favUrl = site => site.hasOwnProperty('favIconUrl') ? site.favIconUrl : 'chrome://favicon/' + site.url
-console.log(favUrl);
 
 const Shortcuts = () => {
     const chrome = useContext(ChromeContext);
@@ -14,7 +13,7 @@ const Shortcuts = () => {
     }, [chrome])
 
     const shortCuts = topSites.map((site) => {
-            return <a className="tab-holder" href={site.url}>
+        return <a className="tab-holder" href={site.url}>
                 <div className="tab-edit-ic">
                     <svg height="20" viewBox="0 0 11 45" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect x="0.201172" y="0.00769043" width="10" height="10" rx="5" fill="white"/>
